ca14f9cf4ed63c1b0bcc77d47d1e2d862eb4479a
fangjq <fangjq20@mails.tsinghua.edu.cn>
2024-05-19 01:05:08 +0800

    error

--- russian/script.js
+++ russian/script.js
@@ -10,36 +10,12 @@ btn.addEventListener("click", () => {
         console.log(doc);
         const russianContent = doc.querySelector('#Russian')
         if (russianContent) {
-            console.log(russianContent.innerHTML);
             const sectionContent = russianContent.parentElement;
             result.innerHTML = sectionContent.outerHTML;
-            console.log(sectionContent.innerHTML);
-            console.log(sectionContent.outerHTML);
         } else {
-            console.log('Not found in Russian!');
-        }
-        // result.innerHTML = `
-        //     <div class="word">
-        //         <h3>${inpWord}</h3>
-        //         <button onclick="playSound()">
-        //             <i class="fa fa-volume-up" style="color: #ac99ff;"></i>
-        //         </button>
-        //     </div>
-        //     <div class="details">
-        //         <p>${data[0].meanings[0].partOfSpeech}</p>
-        //         <p> . ${data[0].phonetic}</p>s
-        //     </div>
-        //     <p class="word-meaning">
-        //         ${data[0].meanings[0].definitions[0].definition}
-        //     </p>
-        //     <p class="word-example">
-        //         ${data[0].meanings[0].definitions[0].example || ""}
-        //     </p>`;
-        // sound.setAttribute("src", `https:${data[0].phonetics[0].audio}`);
-
-    })
-
-        .catch(() => {
             result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
-        });
+        }
+    }).catch(() => {
+        result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
+    });
 });
